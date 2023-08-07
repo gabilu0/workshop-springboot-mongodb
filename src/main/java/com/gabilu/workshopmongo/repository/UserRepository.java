a721ea9b46201e484b78eb0e0b572c5b5e294bb8
Gabriel Santos <gabilubastos@gmail.com>
2023-08-06 21:36:01 -0300

    Conexão com mongodb, repository e service implementados

---
+++ src/main/java/com/gabilu/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gabilu.workshopmongo.repository;
+
+import com.gabilu.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
