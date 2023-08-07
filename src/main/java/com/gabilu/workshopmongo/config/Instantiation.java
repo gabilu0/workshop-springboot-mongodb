a6c98d7972567c4a8731c349aae6b53bf8d7c02c
Gabriel Santos <gabilubastos@gmail.com>
2023-08-06 23:06:09 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/gabilu/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.gabilu.workshopmongo.config;
+
+import com.gabilu.workshopmongo.domain.User;
+import com.gabilu.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration  
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+    }
+}
